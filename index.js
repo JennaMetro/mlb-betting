e8fd20636362c27263d48b85863b7a31774d4453
JennaMetro <jennakop@metropolia.fi>
2019-05-13 13:53:38 +0300

    working

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var data = require('./static/test');
+var io = require('socket.io')(http);
 
-// Global variable to store the latest NHL results
 var latestData;
-// Load the NHL data for when client's first connect
-// This will be updated every 10 minutes
-data.getData().then((result) => { 
+
+data.getData().then((result) => {
     latestData = result;
 });
 
-/*     data.getData().then((result) => { 
-    latestData = result;
-    for(var i in latestData){
-        results.push([i, latestData [i]]);
-        } */
-
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 app.use('/static', express.static(__dirname + '/static'));
@@ -25,6 +18,18 @@ app.use(express.static(__dirname));
 
 
 
-http.listen(4100 || process.env.PORT , function(){
-    console.log('HTTP server started on port 4000' );
+http.listen(4400 || process.env.PORT, function () {
+    console.log('HTTP server started on port 4000');
 });
+io.on('connection', function (socket) {
+    socket.emit('data', latestData);
+});
+
+// refresh once a day
+setInterval(function () {
+    data.getData().then((result) => {
+        latestData = result;
+        io.emit('data', result);
+        console.log('Last updated: ' + new Date());
+    });
+}, 86400000);
--- static/test.js
+++ static/test.js
@@ -1,4 +1,3 @@
-
 var MySportsFeeds = require("mysportsfeeds-node"); 
 var msf = new MySportsFeeds("1.2", true);
 msf.authenticate("a6e9e6be-dfdb-4fca-bae5-afcf75", "Mummeli6");
@@ -6,47 +5,10 @@ msf.authenticate("a6e9e6be-dfdb-4fca-bae5-afcf75", "Mummeli6");
 var today = new Date();
 
 exports.getData = function() {
-    return msf.getData('nhl', '2019-playoff', 'scoreboard', 'json', { 
+    return msf.getData('mlb', '2019-regular', 'scoreboard', 'json', { 
     fordate: today.getFullYear() + 
         ('0' + parseInt(today.getMonth() + 1)).slice(-2) + 
         ('0' + today.getDate()).slice(-2),
     force: true
     });
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function test(){
-    var data = msf.getData( 'mlb', '2016-playoff', 'full_game_schedule', 'json', {});
-    document.getElementById('message').innerHTML = data;
-};
-function toimi(){
-    document.getElementById('message').innerHTML = data;
-} */
-
-/*  function test(){
-    var data = msf.getData( 'mlb', '2016-playoff', 'full_game_schedule', 'json', {});
-    document.getElementById('message').innerHTML = data;
-    console.log(data.gameentry);
-}
-
-
-
-exports.getData = function() {
-        return msf.getData('mlb', '2019-regular', 'scoreboard', 'json', {
-            fordate:20190428,
-            force:true
-        });   
-}; */
-
-
