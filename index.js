0246f06080db73fc42a640ede72be75ad4f2a2ab
JennaMetro <jennakop@metropolia.fi>
2019-05-13 12:41:01 +0300

    install jrender

--- index.js
+++ index.js
@@ -5,22 +5,26 @@ var data = require('./static/test');
 
 // Global variable to store the latest NHL results
 var latestData;
- 
 // Load the NHL data for when client's first connect
 // This will be updated every 10 minutes
 data.getData().then((result) => { 
     latestData = result;
-    console.log('data got ' + latestData)
-    document.getElementById('message').innerHTML = "testailu";
 });
 
-
+/*     data.getData().then((result) => { 
+    latestData = result;
+    for(var i in latestData){
+        results.push([i, latestData [i]]);
+        } */
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 app.use('/static', express.static(__dirname + '/static'));
+app.use(express.static(__dirname));
 
-http.listen(process.env.PORT, function(){
-    console.log('HTTP server started on port 3000');
+
+
+http.listen(4100 || process.env.PORT , function(){
+    console.log('HTTP server started on port 4000' );
 });
--- static/test.js
+++ static/test.js
@@ -1,16 +1,52 @@
-var MySportsFeeds = require("mysportsfeeds-node");
- 
-var msf = new MySportsFeeds("1.2", true, null);
+
+var MySportsFeeds = require("mysportsfeeds-node"); 
+var msf = new MySportsFeeds("1.2", true);
 msf.authenticate("a6e9e6be-dfdb-4fca-bae5-afcf75", "Mummeli6");
  
 var today = new Date();
- 
+
+exports.getData = function() {
+    return msf.getData('mlb', '2019-regular', 'scoreboard', 'json', { 
+    fordate: today.getFullYear() + 
+        ('0' + parseInt(today.getMonth() + 1)).slice(-2) + 
+        ('0' + today.getDate()).slice(-2),
+    force: true
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function test(){
+    var data = msf.getData( 'mlb', '2016-playoff', 'full_game_schedule', 'json', {});
+    document.getElementById('message').innerHTML = data;
+};
+function toimi(){
+    document.getElementById('message').innerHTML = data;
+} */
+
+/*  function test(){
+    var data = msf.getData( 'mlb', '2016-playoff', 'full_game_schedule', 'json', {});
+    document.getElementById('message').innerHTML = data;
+    console.log(data.gameentry);
+}
+
+
+
 exports.getData = function() {
         return msf.getData('mlb', '2019-regular', 'scoreboard', 'json', {
             fordate:20190428,
             force:true
-        });
-        document.getElementById('message').innerHTML = lengthOfName;
-};
+        });   
+}; */
 
 
