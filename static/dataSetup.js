1a7a98af66590c78fb98b9ec599c8892ea1a82dc
Jenna Kopra <jennakop@metropolia.fi>
2019-08-02 21:16:00 +0300

    gets todays games team ids in array

--- static/dataSetup.js
+++ static/dataSetup.js
@@ -3,8 +3,7 @@ var myModule = require('./API_v1_0.js');
 var teamStats = "";
 var scoreboardInformation ="";
 var today = new Date();
-var i = 0;
-var gameArray = [];
+var todaysGameIds = [];
 var fordate = today.getFullYear() + 
 ('0' + parseInt(today.getMonth() + 1)).slice(-2) + 
 ('0' + today.getDate()).slice(-2)
@@ -13,11 +12,19 @@ var fordate = today.getFullYear() +
 teamStats = "seasonal_team_stats-mlb-2019-regular-" + fordate + ".json"
 scoreboardInformation = "scoreboard-mlb-2019-regular-" + fordate + ".json"
 var teamStatsObject = require('../results/'+ teamStats);
-//var teamsPlaying = require('../results/'+ scoreboardInformation);
+var teamsPlaying = require('../results/'+ scoreboardInformation);
 exports.getTodaysJsonData = async function() {
   var temp = Object.values(teamStatsObject);
-  var ArrayOfTeamStats = temp[1];
+  var arrayOfTeamStats = temp[1];
+  var teamsPlayingTemp = Object.values(teamsPlaying);
+  var arrayOfTeamsPlaying = teamsPlayingTemp[0].gameScore;
 
+  arrayOfTeamsPlaying.forEach(game => {
+  todaysGameIds.push(game.game.homeTeam.ID + " " + game.game.awayTeam.ID)
+  //console.log(game.game.awayTeam.ID)
+  //console.log(game.game.homeTeam.ID)
+});
+console.log(todaysGameIds);
 //var games = require('../results/'+ daysGameFilename);
 //var teamsPlaying = require('../results/'+ scoreboardInformation);
     //console.log(games);
@@ -32,5 +39,5 @@ exports.testing = function() {
     //console.log("JOUUUUUU "+ daysGameFilename);
     //console.log("JOUUUUUU "+ teamsPlaying);
     //var teamsPlayingJson = JSON.parse(teamsPlaying);
-    console.log(ArrayOfTeamStats);
+    
 }
