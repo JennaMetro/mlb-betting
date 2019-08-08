0b1bb12a27db8e439db5f2dc72e383af86acd946
Jenna Kopra <jennakop@metropolia.fi>
2019-08-08 19:43:02 +0300

    creates json from days games

--- static/dataSetup.js
+++ static/dataSetup.js
@@ -1,32 +1,31 @@
 var myModule = require('./API_v1_0.js');
 
-var teamStats = "";
+var teamStatsString = "";
 var scoreboardInformation ="";
 var today = new Date();
-var todaysGameIds = [];
 var gameStatsJson = [];
-var homeTeams = [];
-var awayTeams = [];
 var teams = [];
+var tempAway = {}
+var tempHome = {}
+
 var fordate = today.getFullYear() + 
 ('0' + parseInt(today.getMonth() + 1)).slice(-2) + 
 ('0' + today.getDate()).slice(-2)
-
-
-teamStats = "seasonal_team_stats-mlb-2019-regular-" + fordate + ".json"
-scoreboardInformation = "scoreboard-mlb-2019-regular-" + fordate + ".json"
-var teamStatsObject = require('../results/'+ teamStats);
-var teamsPlaying = require('../results/'+ scoreboardInformation);
 exports.getTodaysJsonData = async function() {
+
+teamStatsString = "seasonal_team_stats-mlb-2019-regular-" + fordate + ".json"
+scoreboardInformation = "scoreboard-mlb-2019-regular-" + fordate + ".json"
+var teamStatsObject = require('../results/'+ teamStatsString);
+var teamsPlaying = require('../results/'+ scoreboardInformation);
+
   var temp = Object.values(teamStatsObject);
   var arrayOfTeamStats = temp[1];
   var teamsPlayingTemp = Object.values(teamsPlaying);
   var tempteams  = Object.values(arrayOfTeamStats)
   var arrayOfTeamsPlaying = teamsPlayingTemp[0].gameScore;
-  console.log(tempteams)
-  console.log(arrayOfTeamStats)
-  arrayOfTeamStats.forEach(team => {
-var teamStats = [];
+
+arrayOfTeamStats.forEach(team => {
+var teamStats = {};
 teamStats["ID"] = team.team.id;
 teamStats["battingAvg"] = team.stats.batting.battingAvg;
 teamStats["batterOnBasePct"] = team.stats.batting.batterOnBasePct;
@@ -36,19 +35,35 @@ teamStats["pitchingAvg"] = team.stats.pitching.pitchingAvg;
 teamStats["strikeoutsPer9Innings"] = team.stats.pitching.strikeoutsPer9Innings;
 teamStats["hitsAllowedPer9Innings"] = team.stats.pitching.hitsAllowedPer9Innings;
 teams.push(teamStats);
-  }),
-console.log(teams);
-  arrayOfTeamsPlaying.forEach(game => {
-   
+  })
+
+arrayOfTeamsPlaying.forEach(game => {
+ 
+  for(var i=0; i<=teams.length-1; i++){
+if (teams[i].ID == game.game.homeTeam.ID){
+  homeTeam = teams[i];
+}
+    if (teams[i].ID == game.game.awayTeam.ID){
+      awayTeam = teams[i];
+}
+  
+}
+//build testing.json
+gameStatsJson.push("{ battingAvg: "+ homeTeam.battingAvg + ", batterOnBasePct: " + homeTeam.batterOnBasePct + ", batterSluggingPct: " + homeTeam.batterSluggingPct + ", earnedRunAvg: " + homeTeam.earnedRunAvg + ", pitchingAvg: " + homeTeam.pitchingAvg + ", strikeoutsPer9Innings: " + homeTeam.strikeoutsPer9Innings + ", hitsAllowedPer9Innings: " + homeTeam.hitsAllowedPer9Innings + "AbattingAvg: "+ awayTeam.battingAvg + ", AbatterOnBasePct: " + awayTeam.batterOnBasePct + ", AbatterSluggingPct: " + awayTeam.batterSluggingPct + ", AearnedRunAvg: " + awayTeam.earnedRunAvg + ", ApitchingAvg: " + awayTeam.pitchingAvg + ", AstrikeoutsPer9Innings: " + awayTeam.strikeoutsPer9Innings + ", AhitsAllowedPer9Innings: " + awayTeam.hitsAllowedPer9Innings + "}");
+  //const getFruit = teams.find(team => team.ID === game.game.homeTeam.ID);
+ // let resultObject = search(game.game.homeTeam.ID, teams);
+ 
+  })
+  console.log(gameStatsJson)
+  // gameStatsJson
     //for(var i=0; i<=arrayOfTeamsPlaying.length-1; i++){
       //console.log(game.game.homeTeam.ID = arrayOfTeamStats[i].team.id)
   //  if(game.game.homeTeam.ID = arrayOfTeamStats[i].team.id)
     //  console.log(arrayOfTeamStats[i].team.id)
       //console.log(game.game.homeTeam.ID)
-
-   // }
-  }) 
 }
+   // }
+
 /*     homeTeams.push(game.game.homeTeam.ID)
     awayTeams.push(game.game.awayTeam.ID) */
     
