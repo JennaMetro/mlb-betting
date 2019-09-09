72109982792606baa21a2072cda374b1dc1d1fa6
Jenna Kopra <jennakop@metropolia.fi>
2019-09-09 21:31:16 +0300

    can see winning percentages

--- index.js
+++ index.js
@@ -1,38 +1,66 @@
 var express = require('express');
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
+var ejs = require('ejs');
 var app = express();
 var http = require('http').Server(app);
 var data = require('./static/test');
 var model = require('./static/model')
 var daysGames = require('./static/dataSetup')
 var io = require('socket.io')(http);
-
 var latestData;
- 
-data.getData().then((result) => {
-    latestData = result;
+var newestData;
+
+
+
+
+
+const getInfo = async () => {
+  await data.getData().then((result) => {
+        latestData = result;
+       // console.log(latestData)
+    });
+    
+  await  data.getStatsForToday().then((result) => {
+        todaysStats = result;
+    });
+    await daysGames.getTodaysJsonData();
+    
+    
+   }
+   
+   const fuvk = async () => {
+    await model.createModel().then((result) => {
+       newestData = result;
+       
+   console.log(newestData);
+    });
+}
+
+   getInfo();
+   fuvk();
+   const website = async () => {
+    app.set('view engine', 'ejs');
+    app.engine('html', require('ejs').renderFile); 
+    await app.get('/', function (req, res) {
+        var test = "tjs";
+    res.sendFile(__dirname + '/index.html', {test:test.toString()});
 });
 
-data.getStatsForToday().then((result) => {
-    todaysStats = result;
-});
+ app.use('/static', express.static(__dirname + '/static'));
+ app.use(express.static(__dirname));
 
-daysGames.getTodaysJsonData();
-model.createModel();
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-app.use('/static', express.static(__dirname + '/static'));
-app.use(express.static(__dirname));
-
-http.listen( process.env.PORT || 4400, function () {
+ http.listen( process.env.PORT || 4400, function () {
     console.log('HTTP server started on port 4000');
 });
-io.on('connection', function (socket) {
-    socket.emit('data', latestData);
+ io.on('connection', function (socket) {
+     socket.emit('data', newestData);
 });
+   }
+   website();
+
+
+
 
 // refresh once per hour
 setInterval(function () {
@@ -42,3 +70,12 @@ setInterval(function () {
         console.log('Last updated: ' + new Date());
     });
 }, 3600000);
+
+/* 
+
+
+document.getElementById("predictButton").addEventListener("click", function(){
+        
+    document.getElementById("output").innerHTML = returnResults();
+          });
+ */
--- static/model.js
+++ static/model.js
@@ -1,9 +1,17 @@
-
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 const iris = require('./boston.json');
+const fs = require('fs');
 //const irisTesting = require('./test.json');
 const irisTesting = require('./daysGameData.json');
+var results = "jgjgjgjg";
+var today = new Date();
+var homeWinningPercentage = [];
+var i = 0;
+var fordate = today.getFullYear() + 
+('0' + parseInt(today.getMonth() + 1)).slice(-2) + 
+('0' + today.getDate()).slice(-2)
+
 // Mapping the trainingdata
 const trainingData = tf.tensor2d(iris.map(match=> [
     match.battingAvg,match.batterOnBasePct,match.batterSluggingPct,match.earnedRunAvg,match.pitchingAvg,match.strikeoutsPer9Innings,match.hitsAllowedPer9Innings,match.AbattingAvg,match.AbatterOnBasePct,match.AbatterSluggingPct,match.AearnedRunAvg,match.ApitchingAvg,match.AstrikeoutsPer9Innings,match.AhitsAllowedPer9Innings
@@ -50,16 +58,58 @@ model.compile({
 
 async function train_data(){
     console.log('......Loss History.......');
-    for(let i=0;i<40;i++){
-     let res = await model.fit(trainingData, outputData, {epochs: 40});
+    for(let i=0;i<4;i++){
+     let res = await model.fit(trainingData, outputData, {epochs: 4});
      console.log(`Iteration ${i}: ${res.history.loss[0]}`);
   }
 }
 
 exports.createModel = async function() {
     await train_data(); 
+    
     console.log('....Model Prediction .....')
-    model.predict(testingData).print();
     await model.save('file:///Users/Jenna/Desktop/koulu/3/4/mlb-betting/static/testmodel');
+    
+/*     testArray = model.predict(testingData).dataSync
+    const arr = Array.from(testArray);
+    console.log(testArray);
+    console.log(arr); */
+    const TempPercentageData = model.predict(testingData);
+   PercentageData = TempPercentageData.dataSync(); // get the class of highest probability
+  // console.log(PercentageData + "   a    ");
+  for (i = 0; i < PercentageData.length; i+=2){
+       homeWinningPercentage.push(PercentageData[i])      
+    }
+   // console.log(homeWinningPercentage);
+   var scoreboardInformation = "scoreboard-mlb-2019-regular-" + fordate + ".json"
+    var teamsPlaying = require('../results/'+ scoreboardInformation);
+   //console.log( teamsPlaying)
+    //var teamsPlayingTemp = Object.values(teamsPlaying);
+    //var arrayOfTeamsPlaying = teamsPlaying.scoreboard[0].gameScore;
+    //var teamsPlayingParsed = tempTeamsPlaying[0].gameScore;
+   //console.log(teamsPlaying) 
+    for (i = 0; i <  teamsPlaying.scoreboard.gameScore.length; i++){
+       // console.log(  teamsPlaying.scoreboard.gameScore[i]);
+      // arrayOfTeamsPlaying[i].game.push({"winningPercentage": homeWinningPercentage[i]});
+      teamsPlaying.scoreboard.gameScore[i].game["winningPercentage"] = homeWinningPercentage[i]
+       // homeWinningPercentage.push(PercentageData[i])      
+     }
+    //console.log(teamsPlaying)
+     //fs.writeFileSync("new"+scoreboardInformation, teamsPlaying)
+     var json = JSON.stringify(teamsPlaying);
+     fs.writeFileSync('myjsonfile.json', json, 'utf8');
+     var teamsPlaying2 = require('../myjsonfile.json');
+   //  console.log(teamsPlaying2)
+  
+     return(teamsPlaying2)
+    // console.log(arrayOfTeamsPlaying);
 }
 
+exports.results;
+
+exports.returnResults=function(){
+return results;
+}
+exports.addPercentagesToJson = async function(){
+
+        }
